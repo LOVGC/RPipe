24e253f820ba842d14e9b6d48fbf96d2fd5d4f0a
Yan <yanzhanggc@gmail.com>
2025-04-10 16:59:52 +0800

    add comment

--- src/test_model.py
+++ src/test_model.py
@@ -36,7 +36,10 @@ def runExperiment():
     torch.cuda.manual_seed(cfg['seed'])
     cfg['path'] = os.path.join('output', 'exp') # output/exp 存的是 train and test 的实验结果。
     cfg['tag_path'] = os.path.join(cfg['path'], cfg['tag']) # output/exp/<seed>_<dataset name>_<model_name> 存的是用这个 seed,dataset,model 做实验的结果。
-    cfg['checkpoint_path'] = os.path.join(cfg['tag_path'], 'checkpoint') # 存的是这个实验<seed>_<dataset name>_<model_name>的 checkpoint
+    
+    # 存的是这个实验<seed>_<dataset name>_<model_name>的 checkpoint
+    #     这个 checkpoint 好像是一个 temp checkpoint, 用来暂时存一下 cfg, logger, model, optimizer, scheduler 的状态。
+    cfg['checkpoint_path'] = os.path.join(cfg['tag_path'], 'checkpoint') 
     cfg['best_path'] = os.path.join(cfg['tag_path'], 'best') # 存的是这个实验<seed>_<dataset name>_<model_name>的 best
     cfg['logger_path'] = os.path.join(cfg['tag_path'], 'logger', 'test') # 存的是这个实验<seed>_<dataset name>_<model_name>的 test log
     cfg['result_path'] = os.path.join('output', 'result', cfg['tag']) # 存的是这个实验<seed>_<dataset name>_<model_name>的result
@@ -52,10 +55,10 @@ def runExperiment():
     data_loader = make_data_loader(dataset, cfg[cfg['tag']]['optimizer']['batch_size'])
     test_logger = make_logger(cfg['logger_path'], data_name=cfg['data_name'])
     test(data_loader['test'], model, test_logger)
-    result = resume(cfg['checkpoint_path'])
+    result = resume(cfg['checkpoint_path']) 
     result = {'cfg': cfg, 'logger': {'train': result['logger'],
                                      'test': test_logger.state_dict()}}
-    save(result, cfg['result_path'])
+    save(result, cfg['result_path']) # 存的是 test result, 在 output/result/0_MNIST_linear
     return
 
 
@@ -66,7 +69,11 @@ def test(data_loader, model, logger):
             input_size = input['data'].size(0)
             input = to_device(input, cfg['device'])
             output = model(**input)
+            
+            # 这里 evaluation 是在算在这个 batch 上的平均 loss 和 accuracy
             evaluation = logger.evaluate('test', 'batch', input, output)
+
+            # 然后把这个 batch 的 loss 和 accuracy 加到 logger 里面去
             logger.append(evaluation, 'test', input_size)
             logger.add('test', input, output)
         evaluation = logger.evaluate('test', 'full')
